27f0270a235d65181147b330d0a443072269b002
AmanjotG <74638021+AmanjotG@users.noreply.github.com>
2022-09-07 20:41:36 -0400

    Moved github and linkedin links from Footer to Intro

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,6 @@ function Footer() {
     return (
 
         <footer className="footer" id="footer">
-            <a className="footer-link" href="https://www.linkedin.com/in/amanjot-gulshi/"><i className="fa-brands fa-linkedin fa-xl"></i></a>
-            <a className="footer-link" href="https://github.com/AmanjotG"><i className="fa-brands fa-github fa-xl"></i></a>
             <p>&copy;{year} Amanjot Gulshi</p>
         </footer>
 
